22d5cc6ff7cc5fa99913a6398bdd71c4b938db86
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-11 20:13:42 +0600

    object declaration shorthand

---
+++ object-declaration-shorthand.js
@@ -0,0 +1,29 @@
+const x = 100;
+const y = 200;
+/* const obj = {
+    x : x, 
+    y : y
+} */
+// shortcut 
+const obj = {x, y}
+
+const name = "Kamrujjaman";
+const hobby = "coding";
+
+const rony = {
+    name,
+    hobby,
+}
+// console.log(rony);
+
+/* 
+
+const name = 'Solimullah';
+const area = 'Dhaka';
+
+const nobab = {
+    name: name,
+    area: area
+}
+
+const nobab2 = { name, area }; */
